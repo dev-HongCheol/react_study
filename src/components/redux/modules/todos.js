21b4c43cb336cf3ae10d9c699a4e45e22793fb17
hcchoi <hcchoi@markany.com>
2022-08-03 14:19:04 +0900

    redux upgrade 1

--- src/components/redux/modules/todos.js
+++ src/components/redux/modules/todos.js
@@ -1,34 +1,12 @@
+import { createAction, handleActions } from '@reduxjs/toolkit';
+import produce from 'immer';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
-export const changeInput = (input) => ({
-  type: CHANGE_INPUT,
-  input,
-});
-
 let newId = 3;
-export const insert = (text) => {
-  newId += 1;
-  return ({
-    type: INSERT,
-    todo: {
-      id: newId,
-      text,
-      done: false,
-    },
-  });
-};
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-});
-
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-});
 
 const initialState = {
   input: '',
@@ -45,6 +23,97 @@ const initialState = {
     },
   ],
 };
+
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+
+export const insert = createAction(INSERT, (text) => ({
+  id: newId++,
+  text,
+  done: false,
+}));
+
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
+
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => (
+      produce(state, (draft) => {
+        draft.input = input;
+      })
+    ),
+    [INSERT]: (state, { payload: todo }) => (
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      })
+    ),
+    [TOGGLE]: (state, { payload: id }) => (
+      produce(state, (draft) => {
+        const todo = draft.find((_todo) => _todo.id === id);
+        todo.done = !todo.done;
+      })
+    ),
+    [REMOVE]: (state, { payload: id }) => (
+      produce(state, (drfat) => {
+        const index = drfat.todos.findIndex((_todo) => _todo.id === id);
+        drfat.todos.splice(index, 1);
+      })
+    ),
+  },
+  initialState,
+);
+
+/* const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({
+      ...state,
+      input,
+    }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: todos.map(
+        (todo) => (todo.id === id ? { ...todo, done: !todo.done } : todo),
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState,
+); */
+
+/*
+export const changeInput = (input) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+export const insert = (text) => {
+  newId += 1;
+  return ({
+    type: INSERT,
+    todo: {
+      id: newId,
+      text,
+      done: false,
+    },
+  });
+};
+
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
 
 function todos(state = initialState, action) {
   switch (action.type) {
@@ -75,5 +144,6 @@ function todos(state = initialState, action) {
       return state;
   }
 }
+*/
 
 export default todos;
